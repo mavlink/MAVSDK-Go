8d304e91b56985742252d3536b4ce195f1d9c0fc
Yogesh Khedar <y.khedar@tu-braunschweig.de>
2020-05-30 12:45:04 +0200

    modified the tools/generate_from_protos.bash to generate the files for Go.

--- tools/generate_from_protos.bash
+++ tools/generate_from_protos.bash
@@ -9,7 +9,7 @@ PB_PLUGINS_DIR=${PB_PLUGINS_DIR:-"${SCRIPT_DIR}/../proto/pb_plugins"}
 PROTO_DIR=${PROTO_DIR:-"${SCRIPT_DIR}/../proto/protos"}
 OUTPUT_DIR=${OUTPUT_DIR:-"${SCRIPT_DIR}/../Sources/MAVSDK-Go/Generated"}
 
-PLUGIN_LIST="action calibration camera core gimbal info mission offboard param telemetry"
+PLUGIN_LIST="action core mission geofence telemetry log_files"
 
 if [ ! -d ${PROTO_DIR} ]; then
     echo "Script is not in the right location! It will look for the proto files in '${PROTO_DIR}', which doesn't exist!"
@@ -23,46 +23,13 @@ if [ ! -d ${OUTPUT_DIR} ]; then
     exit 1
 fi
 
-echo ""
 echo "-------------------------------"
 echo "Generating pb and grpc.pb files"
 echo "-------------------------------"
-echo ""
 
-TMP_DIR=${TMP_DIR:-"$(mktemp -d)"}
-echo "Temporary directory for this build: ${TMP_DIR}"
-
-if [ ! -d ${TMP_DIR}/grpc-swift ]; then
-    echo ""
-    echo "--- Cloning grpc-go"
-    echo ""
-
-    git -C ${TMP_DIR} clone https://github.com/jonasvautherin/grpc-swift
-fi
-
-cd ${TMP_DIR}/grpc-swift && make
+GO_GEN_CMD=""
+GO_GEN_RPC_CMD=""
 
 for plugin in ${PLUGIN_LIST}; do
-    protoc ${plugin}.proto -I${PROTO_DIR}/${plugin} --swift_out=${OUTPUT_DIR} --swiftgrpc_out=${OUTPUT_DIR} --swiftgrpc_opt=TestStubs=true --plugin=protoc-gen-swift=${TMP_DIR}/grpc-swift/protoc-gen-swift --plugin=protoc-gen-swiftgrpc=${TMP_DIR}/grpc-swift/protoc-gen-swiftgrpc
-done
-
-echo ""
-echo "-------------------------------"
-echo "Generating the SDK wrappers"
-echo "-------------------------------"
-echo ""
-
-if [ ! -d ${PB_PLUGINS_DIR}/venv ]; then
-    python3 -m venv ${PB_PLUGINS_DIR}/venv
-
-    source ${PB_PLUGINS_DIR}/venv/bin/activate
-    pip install -r ${PB_PLUGINS_DIR}/requirements.txt
-    pip install -e ${PB_PLUGINS_DIR}
-fi
-
-source ${PB_PLUGINS_DIR}/venv/bin/activate
-export TEMPLATE_PATH=${TEMPLATE_PATH:-"${SCRIPT_DIR}/../templates"}
-
-for plugin in ${PLUGIN_LIST}; do
-    protoc ${plugin}.proto --plugin=protoc-gen-custom=$(which protoc-gen-dcsdk) -I${PROTO_DIR}/${plugin} --custom_out=${OUTPUT_DIR} --custom_opt=file_ext=swift
+    protoc ${plugin}.proto -I${PROTO_DIR}/${plugin} --go_out=${OUTPUT_DIR} --gogrpc_out=${OUTPUT_DIR} --plugin=protoc-gen-go=${GO_GEN_CMD} --plugin=protoc-gen-gogrpc=${GO_GEN_RPC_CMD}
 done
