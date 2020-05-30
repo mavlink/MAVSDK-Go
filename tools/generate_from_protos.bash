5e7ea14d285caac76e522d4ec16cd2c22831583a
Yogesh Khedar <y.khedar@tu-braunschweig.de>
2020-05-30 18:26:50 +0200

    some corrections in the sed command.

--- tools/generate_from_protos.bash
+++ tools/generate_from_protos.bash
@@ -15,12 +15,10 @@ PLUGIN_LIST="action core mission geofence telemetry log_files"
 mkdir -p ${PROTO_DIR_TMP}
 cp -r ${PROTO_DIR}/* ${PROTO_DIR_TMP}
 
-sed "/java_package.*/a option go_package = \".;io_mavsdk_$plugin\";" ${PROTO_DIR_TMP}/$plugin/$plugin.proto
-
 for plugin in ${PLUGIN_LIST}; do
-    sed "/java_package.*/a option go_package = \".;io_mavsdk_$plugin\";" ${PROTO_DIR_TMP}/$plugin/$plugin.proto
+    sed -i "/java_package.*/a option go_package = \".;io_mavsdk_$plugin\";" ${PROTO_DIR_TMP}/$plugin/$plugin.proto
     cp ${PROTO_DIR_TMP}/mavsdk_options.proto ${PROTO_DIR_TMP}/$plugin/mavsdk_options.proto 
-    sed "/java_package.*/a option go_package = \".;io_mavsdk_$plugin\";" ${PROTO_DIR_TMP}/$plugin/mavsdk_options.proto
+    sed -i "/java_package.*/a option go_package = \".;io_mavsdk_$plugin\";" ${PROTO_DIR_TMP}/$plugin/mavsdk_options.proto
 done
 
 PROTO_DIR=${PROTO_DIR_TMP}
@@ -47,3 +45,4 @@ GO_GEN_RPC_CMD="/go/bin/protoc-gen-gogrpc"
 for plugin in ${PLUGIN_LIST}; do
     protoc ${plugin}.proto -I${PROTO_DIR}/${plugin} --go_out=${OUTPUT_DIR} --gogrpc_out=${OUTPUT_DIR} --plugin=protoc-gen-go=${GO_GEN_CMD} --plugin=protoc-gen-gogrpc=${GO_GEN_RPC_CMD}
 done
+
