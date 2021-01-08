bfe8f9ace175b86a513a09c5bf0201191817bdfa
shifty21 <yateenderk@gmail.com>
2021-01-09 00:53:40 +0100

    remove dangling function

--- Sources/telemetry/telemetry.pb.go
+++ Sources/telemetry/telemetry.pb.go
@@ -8049,7 +8049,6 @@ func file_telemetry_proto_init() {
 	if File_telemetry_proto != nil {
 		return
 	}
-	file_mavsdk_options_proto_init()
 	if !protoimpl.UnsafeEnabled {
 		file_telemetry_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*SubscribePositionRequest); i {
